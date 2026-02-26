e3e811a0b74fc0b3c0d3703044fe0c89e0be7fd4
Nick Kannenberg <nick.kann2003@gmail.com>
2026-02-26 14:00:48 -0500

    Added skill tree node

--- CozyIncremental/Assets/SkillTreeNode.cs
+++ CozyIncremental/Assets/SkillTreeNode.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class SkillTreeNode : MonoBehaviour
 {
+    private SkillNodeState state;
+    [Header("Item References")]
+    [SerializeField] private GameObject inactive;
+    [SerializeField] private GameObject active;
+    [SerializeField] private GameObject hovered;
+    [SerializeField] private GameObject bought;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,4 +21,44 @@ public class SkillTreeNode : MonoBehaviour
     {
         
     }
+
+    public void SetState(SkillNodeState newState)
+    {
+        if(state == newState) return;
+        DeactivateAll();
+        switch (newState)
+        {
+            case SkillNodeState.INACTIVE:
+                inactive.SetActive(true);
+                break;
+            case SkillNodeState.ACTIVE:
+                active.SetActive(true);
+                break;
+            case SkillNodeState.HOVER:
+                break;
+            case SkillNodeState.SELECTED:
+                hovered.SetActive(true);
+                break;
+            case SkillNodeState.BOUGHT:
+                bought.SetActive(true);
+                break;
+        }
+    }
+
+    private void DeactivateAll()
+    {
+        inactive.SetActive(false);
+        active.SetActive(false);
+        hovered.SetActive(false);
+        bought.SetActive(false);
+    }
+}
+
+public enum SkillNodeState
+{
+    INACTIVE,
+    ACTIVE,
+    HOVER,
+    SELECTED,
+    BOUGHT
 }
